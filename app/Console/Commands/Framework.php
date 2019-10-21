86784c8aed013bbeddabdbbfa4e1601be5dce4ea
benhuang <benhuang1024@gmail.com>
2019-10-21 23:52:11 +0800

    init

---
+++ app/Console/Commands/Framework.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use App\Http\Controllers\FrameworkController;
+
+/**
+ * Class Framework
+ *
+ * @package App\Console\Commands
+ */
+class Framework extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'make:framework
+                            {framework_name : framework name}                            
+                            {--delete : delete framework}
+                            {--D : delete framework}
+                            {--StaticRender : Static rendering html}
+                            {--static : Static rendering html}
+                            ';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * @var array
+     */
+    private $framework_file_types = [
+        'Controller',
+        'Repository',
+        'Service',
+        'Presenter',
+        'Transformer',
+        'Formatter',
+        'Export',
+    ];
+
+    /**
+     * Framework constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     *
+     */
+    public function handle()
+    {
+        try {
+            $framework_name = $this->argument('framework_name');
+            list($is_delete, $is_static_render) = $this->initOption();
+            if ($is_delete && ! $this->confirm('Do you wish to continue? [y|N]')) {
+                throw new Exception('Continue Delete');
+            }
+            $framework_file_types = $this->framework_file_types;
+            $bar = $this->output->createProgressBar(count($framework_file_types));
+            $FrameworkController = new FrameworkController($framework_name);
+            foreach ($framework_file_types as $framework_file_type) {
+                $FrameworkController->handle($framework_file_type, $is_delete, $is_static_render);
+                $bar->advance();
+            }
+            $bar->finish();
+            $msg = 'create';
+            if ($is_delete) {
+                $msg = 'delete';
+            }
+            $stdout_string = PHP_EOL . " {$msg} framework \e[31m{$framework_name}\e[0m \e[32msuccess";
+        } catch (Exception $exception) {
+            $stdout_string = " \e[31m{$exception->getMessage()}\e[0m \e[32min file {$exception->getFile()} line {$exception->getLine()}";
+        }
+        $this->info($stdout_string);
+    }
+
+    /**
+     * @return array
+     */
+    private function initOption(): array
+    {
+        $is_delete = $this->option('delete');
+        $is_delete or $is_delete = $this->option('D');
+        $is_static_render = $this->option('static');
+        $is_static_render or $is_static_render = $this->option('StaticRender');
+        return [$is_delete, $is_static_render];
+    }
+}
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Cache;
+use Str;
+use DB;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+/**
+ * Class Controller
+ *
+ * @package App\Http\Controllers
+ */
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var
+     */
+    protected $service;
+
+
+    /**
+     *
+     */
+    const SUCCESS_CODE = 200;
+
+    /**
+     *
+     */
+    const ERROR_CODE = 500;
+
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @param null $data
+     * @param array $page
+     * @param string $message
+     *
+     * @return array
+     */
+    protected function successReturn($data = null, $page = [], $message = 'success'): array
+    {
+        $response = [
+            'code' => self::SUCCESS_CODE,
+            'msg' => $message,
+            'data' => $data,
+        ];
+        if ($page) {
+            $response['page'] = $page;
+        }
+        return $response;
+    }
+
+    /**
+     * @param \Exception $exception
+     * @param null $type
+     *
+     * @return array|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function catchException(\Exception $exception, $type = null)
+    {
+        $code = self::ERROR_CODE;
+        if ($exception->getCode()) {
+            $code = $exception->getCode();
+        }
+        if ('api' == $type) {
+            return [
+                'code' => $code,
+                'data' => ['error_file' => $exception->getFile() . ':' . $exception->getLine()],
+                'msg' => $exception->getMessage(),
+            ];
+        }
+        return response($exception->getMessage(), $code);
+    }
+
+    /**
+     * @param null $table_name
+     * @param string $connection_name
+     *
+     * @return array
+     */
+    protected function getTableCommentMap($table_name = null, $connection_name = 'mysql'): array
+    {
+        $table_maps = Cache::remember('map_' . $table_name, 1,
+            function () use ($table_name, $connection_name) {
+                if (empty($table_name)) {
+                    $table_name = Str::plural(Str::snake($table_name));
+                }
+                $table_column_dbs = DB::connection($connection_name)->select("show full columns from {$table_name}");
+                $table_columns = array_column($table_column_dbs, 'Comment', 'Field');
+                $filter_words = [
+                    'deleted_by',
+                    'deleted_at',
+                ];
+                foreach ($table_columns as $key => $table_column) {
+                    if (empty($table_column)) {
+                        $table_column = $key;
+                    }
+                    if (!in_array($key, $filter_words)) {
+                        $show_columns[] = [
+                            'prop' => $key,
+                            'label' => $table_column,
+                        ];
+                    }
+                }
+                return serialize($show_columns);
+            });
+        return unserialize($table_maps);
+    }
+
+    /**
+     * @param array $table_comment_map
+     * @param array $child_map_lists
+     *
+     * @return array
+     */
+    protected function appendAssociationModelMap(array $table_comment_map, array $child_map_lists): array
+    {
+        foreach ($child_map_lists as $child_map_list) {
+            $child_maps = [
+                [
+                    'prop' => $child_map_list['prop'],
+                    'label' => $child_map_list['label'],
+                    'is_array' => true,
+                    'child_map' => [
+                        $this->getTableCommentMap($child_map_list['prop']),
+                    ],
+                ],
+            ];
+        }
+        foreach ($child_maps as $child_map) {
+            array_push($table_comment_map, $child_map);
+        }
+        return $table_comment_map;
+    }
+
+}
